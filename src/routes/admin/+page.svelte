2ddcfe7ae26400c2a997f8af06d9e1e16afc886d
KCantu7 <kcantu7@leomail.tamuc.edu>
2023-02-15 16:10:40 -0600

    Added the beginnings of a Firebase library as well as an admin page to test its development.
    These javascript files will have classes used by our main pages to carry out different firebase services.

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,22 @@
+<h1>Admin Testing Page for running js code</h1>
+<a href="/">Back</a>
+
+<script>
+    import {firestoreClass} from "../firebase/crFirestore"
+    const db = new firestoreClass();
+    function writeEntry() {
+        db.createEntry();
+    }
+
+//Old code for testing auth
+//    import {authClass} from "../firebase/crAuth";
+//    
+//    const user = new authClass();
+</script>
+
+
+<div class="container" style="background-color:#f1f1f1">
+      <button type="button" on:click={writeEntry}>Help :D</button>
+</div>
+
+
