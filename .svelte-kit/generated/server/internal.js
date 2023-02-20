c9df9ab691912560efbb2a652a2d08c8fba31809
KCantu7 <kcantu7@leomail.tamuc.edu>
2023-02-20 10:40:38 -0600

    Editted the README to look pretty

--- .svelte-kit/generated/server/internal.js
+++ .svelte-kit/generated/server/internal.js
@@ -2,7 +2,7 @@
 import root from '../root.svelte';
 import { set_assets, set_building, set_private_env, set_public_env, set_version } from '../../../node_modules/@sveltejs/kit/src/runtime/shared.js';
 
-set_version("1676498255751");
+set_version("1676911133615");
 
 export const options = {
 	csp: {"mode":"auto","directives":{"upgrade-insecure-requests":false,"block-all-mixed-content":false},"reportOnly":{"upgrade-insecure-requests":false,"block-all-mixed-content":false}},
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 To install firebase SDK to app:
-npm install firebase
+    npm install firebase
 
     -Firestore needs the following dependencies
         npm install firebase-admin --save
 
 
 To Run App:
-npm install
-npm run dev
+    npm install
+    npm run dev
