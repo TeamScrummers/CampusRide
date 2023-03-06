99ebd74d4bf347cf05d917619dc3099c09d5b93e
ZacheryM <zacktothemax@hotmail.com>
2023-03-06 16:37:38 -0600

    Fixed MapBox-GL Script Loading Delay
    Removed misc deprecated ./map files

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Display navigation directions</title>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-    <link href="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css" rel="stylesheet">
-    <script src="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js"></script>
-    
-    <style>
+<svelte:head>
+  <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css" >
+  <script type="module" 
+    src="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js">
+  </script>
+  <script type="module" 
+    src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.js">
+  </script>
+  <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" type="text/css">
+  <style>
     body { margin: 0; padding: 0; }
     #map { position: absolute; top: 0; bottom: 0; width: 100%; }
     .map-overlay {
@@ -16,38 +16,36 @@
       left: 10px;
       z-index: 1;
     }
-    </style>
-  </head>
+  </style>
+</svelte:head>
 
-  <body>
-    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.js"></script>
 
-    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" type="text/css">
-    
-    <div class = "map-overlay" style="border: 2px solid; background-color: white;">  
-      <a href="/">Back</a>
-    </div>
+<body>
+  <!--  make a container for ui elements
+        pickup location (or geolocate)
+        drop off location (campus/where on campus)
+        latest arrival time
+        Follow Uber's UI/UX flow -->
+  <div class = "map-overlay" style="border: 2px solid; background-color: white;">  
+    <a href="/">Back</a>
+  </div>
 
-    <div id="map"></div>
-    <script>
-        // Should hide api key w/ .env file for future
-        //mapboxgl.accessToken = process.env.PUBLIC_MAPBOX_TOKEN
-        mapboxgl.accessToken = '';
-
-      // conversion from doctype html to proper svelte files make 
-      // map/mapobj to recreate multiple times and error out w/ its assignment.
-      // fix pending
-      const map = new mapboxgl.Map({
+  <div id="map"></div>
+  <script defer>
+    // Should hide api key w/ .env file for future
+    //mapboxgl.accessToken = process.env.PUBLIC_MAPBOX_TOKEN
+      alert("Alert Reached.")
+      mapboxgl.accessToken = '';
+      var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [-96.3344,30.6280 ],
+        center: [-96.3344,30.6280 ], // cstat lat long
         zoom: 11
       });
+
       
       map.addControl(new MapboxDirections({accessToken: mapboxgl.accessToken}),'top-left');
       //map.addControl(geolocation, 'top-right');
-    </script>
-
-  </body>
-
-</html>
+  </script>
+  
+</body>
