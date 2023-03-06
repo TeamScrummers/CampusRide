d224dcbfd0bb223dee2cac83159bdc8bbfd5b90d
ZacheryM <zacktothemax@hotmail.com>
2023-03-06 16:59:29 -0600

    Added Geolocation button to Map View
    Added comments for clarity
    Removed debugging browser alert.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<!-- Loading Mapbox Scripts -->
+<!-- Preloading Mapbox Scripts -->
 <svelte:head>
   <script type="module" src="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js"></script>
   <script type="module" src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.js"></script>
