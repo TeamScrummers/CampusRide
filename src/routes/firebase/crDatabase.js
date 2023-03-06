b66e8b7522ac0b0c1fa87fa91fb6767fa5b9b798
KCantu7 <kcantu7@leomail.tamuc.edu>
2023-03-06 13:20:20 -0600

    Reverted changes to Auth by using original code
    Added original code back into database to ensure account registration is stored in database at execution.

--- src/routes/firebase/crDatabase.js
+++ src/routes/firebase/crDatabase.js
@@ -1,13 +1,42 @@
 import { app } from "./initialFirebase"
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, get, child, update } from "firebase/database";
 
 const database = getDatabase(app);
+export let readDb;
 
-
-export function writeToDatabase(location, data){
-    set(ref(database, location), data)
+//Create Operation
+//  Location needs to be in format, without angle brackets: "<table>/" + <key>
+//        location: "users/" + userID
+//  Data needs to be in format, { value1, value2, value3...}
+//        data: { email: "email@email.com", password: "psswrd" }
+export function CreateFromDatabase(path, data){
+    set(ref(database, path), data);
+    console.log("Data written");
 }
 
-export function readFromDatabaseOnValue(location, data){
-    onValue(ref(database, ))
+//Read operation
+//  Location needs to be in format, without angle brackets: "<table>/<pk>
+//          location: users/userID
+export function readFromDatabaseGet(path){
+    get(child(ref(database), "user/userId")).then((snapshot) => {
+        if(snapshot.exists()){
+            console.log(snapshot.val());
+        } else {
+            console.log("No Data avilable");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
 }
+
+//Update operation
+export function updateFromDatabase(path, data){
+    update(ref(database, path), data)
+}
+
+//export function readFromDatabaseOnValue(location){
+//    onValue(ref(database, location), (snapshot) => {
+//        const data = snapshot.val();
+//        readDb = 4;
+//    })
+//}
