cd9de2b168f214b43695c296bafea5a335d918a2
KCantu7 <kcantu7@leomail.tamuc.edu>
2023-03-07 12:03:28 -0600

    Added crTrip and crDrivers for tasks 1.2 and 1.4
    We now have most of the back end ready to hard code a prototype

---
+++ src/routes/firebase/crDrivers.js
@@ -0,0 +1,6 @@
+import { searchFromDatabase } from "./crDatabase";
+
+export async function provideDriverList(){
+    const result = Object.keys(await searchFromDatabase("drivers", "firstName", "Kolby"))
+    console.log(result[0]) 
+}
