b947704158d4e0402a4c8e4dceeaf2350abdb86f
KCantu7 <kcantu7@leomail.tamuc.edu>
2023-03-20 10:45:07 -0500

    Pilot app

--- src/routes/map/locateuser.svelte
+++ src/routes/map/locateuser.svelte
@@ -29,13 +29,13 @@ function geoFindMe() {
     }
   
     function error() {
-      status.textContent = "Unable to retrieve your location";
+      //status.textContent = "Unable to retrieve your location";
     }
   
     if (!navigator.geolocation) {
       status.textContent = "Geolocation is not supported by your browser";
     } else {
-      status.textContent = "Locating…";
+      //status.textContent = "Locating…";
       navigator.geolocation.getCurrentPosition(success, error);
     }
   }
--- src/routes/trippickup/+page.svelte
+++ src/routes/trippickup/+page.svelte
@@ -9,13 +9,30 @@
   
 </script>
 
+
 <section>
   <div class = "map-overlay">
+    <!--
     Destination Coords: {$destinationCoords}
     <br>
     Time: {$latestArrival}
     <br>
     User Coords: {$userCoords}
+    <br>
+    Driver coords: {driverLocation}
+    -->
+    <h1>On the way</h1>
   </div>
 </section>
 <DirectionsDriver></DirectionsDriver>
+
+<style>
+  .map-overlay{
+      background-color: rgb(209, 209, 209);
+        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        position:relative;
+        top: 400px;
+        height: 100px;
+        width: 70%
+  }
+</style>
