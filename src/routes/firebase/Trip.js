bd8872086ee9cebf80489570d795b27c5d45b426
ZacheryM <zacktothemax@hotmail.com>
2023-04-11 22:37:25 -0500

    Moved /matching/Trip.js to /firebase/Trip.js

--- src/routes/firebase/Trip.js
+++ src/routes/firebase/Trip.js
@@ -9,3 +9,52 @@ export function updateTrip(tripId, data){
         driverId: data
     });
 }
+
+export class Trip {
+    constructor(tripId, driver, passengers, startLocation, endLocation, fare, date) {
+      this.tripId = tripId;
+      this.driver = driver;
+      this.passengers = passengers;
+      this.startLocation = startLocation;
+      this.endLocation = endLocation;
+      this.fare = fare;
+      this.date = date;
+    }
+
+    /**
+     * @brief Takes an array of two user objects to make a trip object out of.
+     * @param {object} driver - User object that represents the driver
+     * @param {object} passenger - User object that represents the passenger
+    */
+    makeTrip(driver, passenger) {
+      // Code
+    }
+    // Convert Trip object to JSON data
+    toJSON() {
+      return {
+        tripId: this.tripId,
+        driver: this.driver.toJSON(), // calling User.js toJSON()
+        passengers: this.passengers.map((passenger) => passenger.toJSON()), // calling User.js toJSON()
+        startLocation: this.startLocation,
+        endLocation: this.endLocation,
+        fare: this.fare,
+        date: this.date,
+      };
+    }
+  
+    // Create Trip object from JSON data
+    static fromJSON(data) {
+      const driver = User.fromJSON(data.driver); // calling User.js toJSON()
+      const passengers = data.passengers.map((passenger) => User.fromJSON(passenger)); // calling User.js fromJSON()
+      const trip = new Trip(
+        data.tripId,
+        driver,
+        passengers,
+        data.startLocation,
+        data.endLocation,
+        data.fare,
+        data.date
+      );
+      return trip;
+    }
+  }
