ef35c78229f2dede132d0b96c021b5e9df605bc8
Kolby Cantu <kcantu7@leomail.tamuc.edu>
2023-04-11 22:26:59 -0500

    Added notification testing

--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -1,8 +1,12 @@
 <script>
-    import { requestPermission } from '../firebase/Notifications'
+    import {sendTheUserAPushNotifcation, sendDriverArrivedNotifcation, sendPassengerAvailableNotifcation, sendDriverAcceptedNotifcation} from "../firebase/PushNotifications"
 </script>
 
 <main>
 	<h1>Notifications Test Page</h1>
 	<a href="/">Back</a>
+	<button on:click={sendTheUserAPushNotifcation}>Punch Yourself</button>
+	<button on:click={sendDriverAcceptedNotifcation}>Driver Accepted</button>
+	<button on:click={sendDriverArrivedNotifcation}>Driver Arrived</button>
+	<button on:click={sendPassengerAvailableNotifcation}>Passenger Available</button>
 </main>
