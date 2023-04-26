388bf0f6a4cebfd44a875d9284e949bf61a9dad4
ZacheryM <zacktothemax@hotmail.com>
2023-04-26 03:53:11 -0500

    Accept Page Displays 1 Passenger & Range Slider
    Added a slider component in src/routes/accept/Slider.svelte
    Updated routeCalculation.js for better fare calculation, includes case where user is very close to driver and defaults to a minimum payment.
    Misc. Changes to accept page to allow for one passenger display & range slider. Range slider allows driver to set maximum drive distance to filter available passengers. Defaults to fifty.

---
+++ src/routes/accept/Slider.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  
+  export let value = 50;
+  const dispatch = createEventDispatcher();
+
+  const handleChange = (event) => {
+    value = event.target.value;
+    if (value === '100') {
+      event.target.max = 'Infinity';
+    }
+    dispatch('valueChanged', value);
+  };
+</script>
+
+<div class="slider-container">
+  <input
+    type="range"
+    min="0"
+    max="100"
+    value={value}
+    on:input={handleChange}
+    class="slider"
+  />
+  <p class="slider-value">{value === '100' ? '∞' : value}</p>
+</div>
+
+<style>
+  .slider-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .slider {
+    width: 200px;
+    margin: 0 10px;
+  }
+
+  .slider-value {
+    font-size: 18px;
+    font-weight: bold;
+  }
+</style>
