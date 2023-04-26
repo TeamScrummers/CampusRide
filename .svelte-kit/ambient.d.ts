8844d5e3fe725ac07f10a30e401e320bb0bba097
Bayspeed <91317132+Bayspeed@users.noreply.github.com>
2023-04-25 22:13:03 -0500

    Removed price ticker from home

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -26,10 +26,7 @@
  * ```
  */
 declare module '$env/static/private' {
-<<<<<<< HEAD
 	export const ACSvcPort: string;
-=======
->>>>>>> matchmaking
 	export const ALLUSERSPROFILE: string;
 	export const APPDATA: string;
 	export const ChocolateyInstall: string;
@@ -43,15 +40,11 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-<<<<<<< HEAD
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const FP_NO_HOST_CHECK: string;
 	export const GIT_ASKPASS: string;
 	export const GNUPLOT_LIB: string;
-=======
-	export const GIT_ASKPASS: string;
->>>>>>> matchmaking
 	export const HOME: string;
 	export const HOMEDRIVE: string;
 	export const HOMEPATH: string;
@@ -85,13 +78,9 @@ declare module '$env/static/private' {
 	export const npm_package_version: string;
 	export const NPM_PREFIX_NPM_CLI_JS: string;
 	export const NUMBER_OF_PROCESSORS: string;
-<<<<<<< HEAD
 	export const OculusBase: string;
 	export const OneDrive: string;
 	export const OneDriveCommercial: string;
-=======
-	export const OneDrive: string;
->>>>>>> matchmaking
 	export const OneDriveConsumer: string;
 	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
 	export const OS: string;
@@ -107,10 +96,7 @@ declare module '$env/static/private' {
 	export const PROMPT: string;
 	export const PSModulePath: string;
 	export const PUBLIC: string;
-<<<<<<< HEAD
 	export const RlsSvcPort: string;
-=======
->>>>>>> matchmaking
 	export const SESSIONNAME: string;
 	export const SystemDrive: string;
 	export const SystemRoot: string;
@@ -122,11 +108,7 @@ declare module '$env/static/private' {
 	export const USERDOMAIN_ROAMINGPROFILE: string;
 	export const USERNAME: string;
 	export const USERPROFILE: string;
-<<<<<<< HEAD
 	export const VBOX_MSI_INSTALL_PATH: string;
-=======
-	export const VS160COMCOMNTOOLS: string;
->>>>>>> matchmaking
 	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
@@ -162,10 +144,7 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
-<<<<<<< HEAD
 		ACSvcPort: string;
-=======
->>>>>>> matchmaking
 		ALLUSERSPROFILE: string;
 		APPDATA: string;
 		ChocolateyInstall: string;
@@ -179,15 +158,11 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-<<<<<<< HEAD
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		FP_NO_HOST_CHECK: string;
 		GIT_ASKPASS: string;
 		GNUPLOT_LIB: string;
-=======
-		GIT_ASKPASS: string;
->>>>>>> matchmaking
 		HOME: string;
 		HOMEDRIVE: string;
 		HOMEPATH: string;
@@ -221,13 +196,9 @@ declare module '$env/dynamic/private' {
 		npm_package_version: string;
 		NPM_PREFIX_NPM_CLI_JS: string;
 		NUMBER_OF_PROCESSORS: string;
-<<<<<<< HEAD
 		OculusBase: string;
 		OneDrive: string;
 		OneDriveCommercial: string;
-=======
-		OneDrive: string;
->>>>>>> matchmaking
 		OneDriveConsumer: string;
 		ORIGINAL_XDG_CURRENT_DESKTOP: string;
 		OS: string;
@@ -243,10 +214,7 @@ declare module '$env/dynamic/private' {
 		PROMPT: string;
 		PSModulePath: string;
 		PUBLIC: string;
-<<<<<<< HEAD
 		RlsSvcPort: string;
-=======
->>>>>>> matchmaking
 		SESSIONNAME: string;
 		SystemDrive: string;
 		SystemRoot: string;
@@ -258,11 +226,7 @@ declare module '$env/dynamic/private' {
 		USERDOMAIN_ROAMINGPROFILE: string;
 		USERNAME: string;
 		USERPROFILE: string;
-<<<<<<< HEAD
 		VBOX_MSI_INSTALL_PATH: string;
-=======
-		VS160COMCOMNTOOLS: string;
->>>>>>> matchmaking
 		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 		VSCODE_GIT_ASKPASS_MAIN: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
